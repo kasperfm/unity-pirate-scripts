bfafef607f6c3c02ff2577d06bea4604114caed5
Kasper Færch Mikkelsen <kasper.faerch@gmail.com>
2015-10-28 18:59:09 +0100

    Movement3D and Jump3D script is made. But lacks comments so far...

---
+++ unity-pirate-scripts/Assets/3D/Scripts/Player/Jump3D.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jump3D : MonoBehaviour {
+
+	[Header("Knapper")]
+	public KeyCode jumpButton = KeyCode.Space;
+
+	[Header("Hoppe indstillinger")]
+	public float jumpHeight = 5.0f;
+	
+	private Rigidbody rb;
+
+	void Start(){
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	void Update(){
+		Jump ();
+	}
+
+	void Jump(){
+		if (Input.GetKeyDown (jumpButton)) {
+			rb.AddForce(Vector3.up * jumpHeight, ForceMode.VelocityChange);
+		}
+	}
+}
---
+++ unity-pirate-scripts/Assets/3D/Scripts/Player/Movement3D.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum PlayerObjectType{
+	Ball, Cube, Model
+}
+
+public class Movement3D : MonoBehaviour {
+
+	private Rigidbody rb;
+	private float origMoveSpeed;
+
+	[Header("Indstillinger")]
+	public PlayerObjectType playerObjectType = PlayerObjectType.Ball;
+	public float moveSpeed = 3.0f;
+	public Vector3 startPosition = new Vector3();
+	public float resetUnderYAxis = -4.0f;
+	public bool multipleDirections = true;
+
+	[Header("Knapper")]
+	public KeyCode moveForward	 = KeyCode.UpArrow;
+	public KeyCode moveBackwards = KeyCode.DownArrow;
+	public KeyCode moveLeft		 = KeyCode.LeftArrow;
+	public KeyCode moveRight	 = KeyCode.RightArrow;
+	public KeyCode runButton	 = KeyCode.LeftShift;
+
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		origMoveSpeed = moveSpeed;
+	}
+
+	public void ResetPosition(Vector3 position){
+		rb.isKinematic = true;
+		transform.position = position;
+		rb.isKinematic = false;
+	}
+	
+	void Update ()
+	{
+		if (transform.position.y <= resetUnderYAxis) {
+			ResetPosition(startPosition);
+		}
+
+		Vector3 movement = new Vector3 ();
+
+		if (Input.GetKey (moveForward)) {
+			if(multipleDirections){
+				movement += Vector3.forward;
+			}else{
+				movement = Vector3.forward;
+			}
+		}
+		
+		if (Input.GetKey (moveBackwards)) {
+			if(multipleDirections){
+				movement += Vector3.back;
+			}else{
+				movement = Vector3.back;
+			}
+		}
+		
+		if (Input.GetKey (moveLeft)) {
+			if(multipleDirections){
+				movement += Vector3.left;
+			}else{
+				movement = Vector3.left;
+			}
+		}
+		
+		if (Input.GetKey (moveRight)) {
+			if(multipleDirections){
+				movement += Vector3.right;
+			}else{
+				movement = Vector3.right;
+			}
+		}
+
+		if (Input.GetKey (runButton)) {
+			moveSpeed = origMoveSpeed * 6;
+		} else {
+			moveSpeed = origMoveSpeed;
+		}
+
+		if (playerObjectType == PlayerObjectType.Ball) {
+			rb.AddForce (movement * moveSpeed, ForceMode.VelocityChange);
+		} else if (playerObjectType == PlayerObjectType.Cube && movement != Vector3.zero) {
+			transform.Translate((movement * moveSpeed) * Time.deltaTime, Space.World);
+		}
+	}
+}
