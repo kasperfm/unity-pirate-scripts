84e40332fee706e98a9b518133e69b0fcbf4ee37
Kasper Færch Mikkelsen <kasper.faerch@gmail.com>
2015-11-10 20:07:20 +0100

    Updated scripts with comments.

--- unity-pirate-scripts/Assets/3D/Scripts/Player/Jump3D.cs
+++ unity-pirate-scripts/Assets/3D/Scripts/Player/Jump3D.cs
@@ -1,27 +1,47 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿/*
+    Dette script får spilleren til at hoppe ved tryk på en valgfri tast.
+    Skrevet af: Kasper Færch Mikkelsen - Coding Pirates Viborg
+*/
+
+using UnityEngine;
+
+// Der skal være et Rigidbody komponent på objektet for at dette script kan fungere.
+[RequireComponent(typeof(Rigidbody))]
 
 public class Jump3D : MonoBehaviour {
 
+    /// <summary>
+    /// Den knap der får spilleren til at hoppe.
+    /// </summary>
 	[Header("Knapper")]
 	public KeyCode jumpButton = KeyCode.Space;
 
+    /// <summary>
+    /// Højden på et enkelt hop.
+    /// </summary>
 	[Header("Hoppe indstillinger")]
 	public float jumpHeight = 5.0f;
-	
-	private Rigidbody rb;
 
-	void Start(){
+    // Variabel til at gemme objektets rigidbody i.
+    private Rigidbody rb;
+
+    // Dette bliver kørt når spillet starter.
+    void Start(){
 		rb = GetComponent<Rigidbody> ();
 	}
 
-	void Update(){
-		Jump ();
+    // Køre denne funktion hele tiden i spillet (hver frame).
+    void Update(){
+        // Tjekker om der er trykket på hoppe-knappen.
+        if (Input.GetKeyDown(jumpButton)) {
+            Jump();
+        }
 	}
 
+    /// <summary>
+    /// Får spilleren til at hoppe.
+    /// </summary>
 	void Jump(){
-		if (Input.GetKeyDown (jumpButton)) {
-			rb.AddForce(Vector3.up * jumpHeight, ForceMode.VelocityChange);
-		}
+        rb.AddForce(Vector3.up * jumpHeight, ForceMode.VelocityChange);
 	}
 }
--- unity-pirate-scripts/Assets/3D/Scripts/Player/Movement3D.cs
+++ unity-pirate-scripts/Assets/3D/Scripts/Player/Movement3D.cs
@@ -1,48 +1,106 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿/*
+    Formålet med dette script er at håndtere spillerens bevægelse.
+    Skrevet af: Kasper Færch Mikkelsen - Coding Pirates Viborg
+*/
 
+using UnityEngine;
+
+// Definerer de forskellige typer spilleren kan være.
 public enum PlayerObjectType{
 	Ball, Cube, Model
 }
 
+// Der skal være et Rigidbody komponent på objektet for at dette script kan fungere.
+[RequireComponent(typeof(Rigidbody))]
+
 public class Movement3D : MonoBehaviour {
 
+    // Variabel til at gemme objektets rigidbody i.
 	private Rigidbody rb;
+
+    // Den originale bevægelses hastighed bliver gemt når spillet starter.
 	private float origMoveSpeed;
 
-	[Header("Indstillinger")]
+    /// <summary>
+    /// Den type dit spiller-objekt er.
+    /// </summary>
+    [Header("Indstillinger")]
 	public PlayerObjectType playerObjectType = PlayerObjectType.Ball;
-	public float moveSpeed = 3.0f;
-	public Vector3 startPosition = new Vector3();
-	public float resetUnderYAxis = -4.0f;
-	public bool multipleDirections = true;
 
-	[Header("Knapper")]
-	public KeyCode moveForward	 = KeyCode.UpArrow;
-	public KeyCode moveBackwards = KeyCode.DownArrow;
-	public KeyCode moveLeft		 = KeyCode.LeftArrow;
-	public KeyCode moveRight	 = KeyCode.RightArrow;
+    /// <summary>
+    /// Den hastighed du bevæger dig med.
+    /// </summary>
+    public float moveSpeed = 3.0f;
+
+    /// <summary>
+    /// Spillerens start position.
+    /// </summary>
+    public Vector3 startPosition = new Vector3();
+
+    /// <summary>
+    /// Når spilleren er under dette punkt på Y-aksen, starter man forfra.
+    /// </summary>
+    public float resetUnderYAxis = -4.0f;
+
+    /// <summary>
+    /// Skal man kunne bevæge sig i flere retninger samtidig?
+    /// </summary>
+    public bool multipleDirections = true;
+
+    /// <summary>
+    /// Gå fremad.
+    /// </summary>
+    [Header("Knapper")]
+    public KeyCode moveForward	 = KeyCode.UpArrow;
+
+    /// <summary>
+    /// Gå baglæns.
+    /// </summary>
+    public KeyCode moveBackwards = KeyCode.DownArrow;
+
+    /// <summary>
+    /// Gå til venstre.
+    /// </summary>
+    public KeyCode moveLeft		 = KeyCode.LeftArrow;
+
+    /// <summary>
+    /// Gå til højre.
+    /// </summary>
+    public KeyCode moveRight	 = KeyCode.RightArrow;
+
+    /// <summary>
+    /// Hold denne knap nede for at løbe.
+    /// </summary>
 	public KeyCode runButton	 = KeyCode.LeftShift;
 
+    // Dette bliver kørt når spillet starter.
 	void Start () {
 		rb = GetComponent<Rigidbody>();
 		origMoveSpeed = moveSpeed;
 	}
 
+    /// <summary>
+    /// Flytter spilleren til start positionen.
+    /// </summary>
+    /// <param name="position">Start position</param>
 	public void ResetPosition(Vector3 position){
 		rb.isKinematic = true;
 		transform.position = position;
 		rb.isKinematic = false;
 	}
 	
+    // Køre denne funktion hele tiden i spillet (hver frame).
 	void Update ()
 	{
+        // Tjekker om spilleren er under et bestemt punkt på Y-aksen.
 		if (transform.position.y <= resetUnderYAxis) {
 			ResetPosition(startPosition);
 		}
 
+        // Variabel til at gemme den retning du vil bevæge dig i.
 		Vector3 movement = new Vector3 ();
 
+        // Tryk på fremad knappen.
 		if (Input.GetKey (moveForward)) {
 			if(multipleDirections){
 				movement += Vector3.forward;
@@ -51,6 +109,7 @@ public class Movement3D : MonoBehaviour {
 			}
 		}
 		
+        // Tryk på baglæns knappen.
 		if (Input.GetKey (moveBackwards)) {
 			if(multipleDirections){
 				movement += Vector3.back;
@@ -59,6 +118,7 @@ public class Movement3D : MonoBehaviour {
 			}
 		}
 		
+        // Tryk på venstre knappen.
 		if (Input.GetKey (moveLeft)) {
 			if(multipleDirections){
 				movement += Vector3.left;
@@ -67,6 +127,7 @@ public class Movement3D : MonoBehaviour {
 			}
 		}
 		
+        // Tryk på højre knappen.
 		if (Input.GetKey (moveRight)) {
 			if(multipleDirections){
 				movement += Vector3.right;
@@ -75,12 +136,16 @@ public class Movement3D : MonoBehaviour {
 			}
 		}
 
+        // Tjekker om løbe-knappen er holdt nede.
+        // Hvis ja, så ganger vi hastigheden med 6.
 		if (Input.GetKey (runButton)) {
 			moveSpeed = origMoveSpeed * 6;
 		} else {
 			moveSpeed = origMoveSpeed;
 		}
 
+        // Bruger force til at bevæge spilleren med hvis du er en bold.
+        // Ellers bevæg dig normalt.
 		if (playerObjectType == PlayerObjectType.Ball) {
 			rb.AddForce (movement * moveSpeed, ForceMode.VelocityChange);
 		} else if (playerObjectType == PlayerObjectType.Cube && movement != Vector3.zero) {
