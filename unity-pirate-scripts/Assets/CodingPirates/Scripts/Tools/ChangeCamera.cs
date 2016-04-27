a112ebd784d4ac06fde2b5e96e004b80ad11fccf
Kasper Færch Mikkelsen <kasper.faerch@gmail.com>
2016-04-27 19:21:48 +0200

    Made a CameraChanger script, to change cameras on-the-fly.

---
+++ unity-pirate-scripts/Assets/CodingPirates/Scripts/Tools/ChangeCamera.cs
@@ -0,0 +1,35 @@
+﻿/*
+    Dette script kan skifte mellem forskellige kameraer i scenen.
+    Skrevet af: Kasper Færch Mikkelsen - Coding Pirates Viborg
+*/
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ChangeCamera : MonoBehaviour {
+	public List<Camera> cameras = new List<Camera>();
+	private int currentCamera = 0;
+
+	public KeyCode shiftButton = KeyCode.Tab;
+
+	void Update () {
+		if(Input.GetKeyUp(shiftButton)){
+			if(cameras.Count > 1){
+				for(int i = 0; i < cameras.Count; i++){
+					if(i == currentCamera){
+						cameras[i].enabled = true;
+					}else{
+						cameras[i].enabled = false;
+					}
+				}
+
+				if(currentCamera >= cameras.Count -1){
+					currentCamera = 0;
+				}else{
+					currentCamera++;
+				}
+			}
+		}
+	}
+}
